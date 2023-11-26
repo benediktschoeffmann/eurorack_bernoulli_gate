141b764b42e6ec3a9633ce519fb16a33db401ba7
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2023-11-26 16:28:59 +0100

    Change: use 2 differents ports for jacks and leds, add doku

--- BRANCHES.ino
+++ BRANCHES.ino
@@ -1,25 +1,35 @@
-#define KNOB_PIN  A0
-#define IN_PIN    2
-#define OUT_A_PIN 3
-#define OUT_B_PIN 4
+#define KNOB_PIN  A0  // potentionmeter
+#define IN_AN_PIN A1  // trigger-in jack
+#define IN_PIN    2   // trigger-in button
 
-#define RESOLUTION 1024
-#define DEBOUNCE 35
+#define LED_A_PIN 3   // left led
+#define LED_B_PIN 4   // right led
+
+#define OUT_A_PIN 5   // left out-jack
+#define OUT_B_PIN 6   // right out-jack
+
+
+#define RESOLUTION 1024 // ADC-Resolution
+#define TRESHOLD 768    // for High State
+#define DEBOUNCE 50     // in ms
+
 bool pressed;
-int p, r;
-
+uint8_t p, r;
 
 void setup() {
   pressed = false;
   randomSeed(analogRead(A4));
   pinMode(IN_PIN, INPUT_PULLUP);
+  pinMode(IN_AN_PIN, INPUT_PULLUP);
+  pinMode(LED_A_PIN, OUTPUT);
+  pinMode(LED_B_PIN, OUTPUT);
   pinMode(OUT_A_PIN, OUTPUT);
   pinMode(OUT_B_PIN, OUTPUT);
 }
 
 void loop() {
 
-  if (LOW == digitalRead(IN_PIN)) {
+  if (LOW == digitalRead(IN_PIN) || TRESHOLD < analogRead(IN_AN_PIN)) {
 
     if (pressed == false) {
       pressed = true;
@@ -28,8 +38,11 @@ void loop() {
       
       if (r <= p) {
         digitalWrite(OUT_A_PIN, HIGH);
+        digitalWrite(LED_A_PIN, HIGH);
+
       } else {
         digitalWrite(OUT_B_PIN, HIGH);
+        digitalWrite(LED_B_PIN, HIGH);
       }
       delay(DEBOUNCE);
     } else {
@@ -40,6 +53,9 @@ void loop() {
     pressed = false;
     digitalWrite(OUT_A_PIN, LOW);
     digitalWrite(OUT_B_PIN, LOW);
+    digitalWrite(LED_A_PIN, LOW);
+    digitalWrite(LED_B_PIN, LOW);
+    
     delay(DEBOUNCE);
   }
   
